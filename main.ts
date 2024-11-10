9124202610b04278400bc1c1d7fb0dc081c222ea
Thorsten <20698311+panikperformance@users.noreply.github.com>
2024-11-10 10:36:10 +0100

    knopf a misst temp, peep bestätigt, knopf b zeigt temp auf 5x5led

--- main.ts
+++ main.ts
@@ -1,3 +1,13 @@
-basic.forever(function () {
-	
+let temperaturWert = 0
+
+
+input.onButtonPressed(Button.A, function () {
+       temperaturWert = input.temperature()
+   
+   music.playTone(262, music.beat(BeatFraction.Half))
+})
+
+
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(temperaturWert)
 })
